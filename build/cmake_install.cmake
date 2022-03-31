18186e8df02ac62e61a9c1b908054ef137f4a8bb
HanPollo <claudio.zamorano@ing.uchile.cl>
2022-03-31 18:09:28 -0300

    Glad and GLFW implemented
    glad and glfw libraries and window creation

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/pollo/OneDrive/Documentos/U/Arqui Motores de Juego/BoomBoomEngine/Test/source
+# Install script for directory: C:/Users/pollo/OneDrive/Documentos/U/Arqui Motores de Juego/BoomBoomEngine/source
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/hello_world_project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/main_project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/pollo/OneDrive/Documentos/U/Arqui Motores de Juego/BoomBoomEngine/Test/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/pollo/OneDrive/Documentos/U/Arqui Motores de Juego/BoomBoomEngine/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ source/console_print.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "console_print.h"
+
+void print_welcome() {
+	std::cout << "Welcome to Boom Boom Engine!\n" << "Here you can create Rhythm Games ;)";
+}
